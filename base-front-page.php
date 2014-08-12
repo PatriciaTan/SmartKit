bd39d832af06d20205e584c6c21ad84dcd1dd700
Mason L <mason.lawlor@me.com>
2014-08-12 10:59:59 -0700

    try again

--- base-front-page.php
+++ base-front-page.php
@@ -33,23 +33,6 @@
 
   <div class="supreme-container">
 
-  <div class="wrap container" role="document">
-    <div class="content row">
-      <main class="main <?php echo roots_main_class(); ?>" role="main">
-        <?php include roots_template_path(); ?>
-      </main><!-- /.main -->
-      <?php if (roots_display_sidebar()) : ?>
-        <aside class="sidebar <?php echo roots_sidebar_class(); ?>" role="complementary">
-          <?php include roots_sidebar_path(); ?>
-        </aside><!-- /.sidebar -->
-      <?php endif; ?>
-    </div><!-- /.content -->
-  </div><!-- /.wrap -->
-
-  <?php get_template_part('templates/footer'); ?>
-
-    </div> <!-- SUPREME -->
-
 <script language="JavaScript">
   <!--
     function _submit_form(frm){
