de6c83b6506efd01a77559c1638c9da42cce7067
Mason L <mason.lawlor@me.com>
2014-08-13 15:48:23 -0700

    fixed header- tried to update video, still no luck

--- base-front-page.php
+++ base-front-page.php
@@ -7,60 +7,17 @@
     </div>
   <![endif]-->
 
-<header class="banner navbar navbar-default navbar-fixed-top" role="banner">
-  <div class="container">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="<?php echo home_url(); ?>/"><img src="http://masonlawlor.com//smartkit/assets/images/logo.png" alt="logo-nav" width="auto" height="48px" /></a>
-    </div>
-
-    <nav class="collapse navbar-collapse" role="navigation">
-    <button type="button" class="pull-right btn btn-default security-btn">Back on Indiegogo</button>
-    <ul class="nav navbar-nav navbar-right">
-      <li><a title="Lightning fast" href="#home" class="security-nav scroll">Home Security</a></li>
-      <li><a title="Get in touch!" href="#energy" class="scroll energy-nav">Smart Energy</a></li>
-      <li><a title="External page sample" href="#auto" class="scroll auto-nav">Home Automation</a></li>
-    </ul>
-    </nav>
-  </div>
-</header>
-
-
-<div class="wrap container">
-  <div class="col-sm-8 col-sm-offset-2"><!-- Video -->
-	  <div class="row"> 
-	    <center>
-	      <iframe onClick="ga('send', 'event', 'video', 'play', 'main video');" src="//fast.wistia.net/embed/iframe/ayvzkgovfs?videoFoam=true" allowtransparency="true" frameborder="0" scrolling="no" class="wistia_embed" name="wistia_embed" allowfullscreen mozallowfullscreen webkitallowfullscreen oallowfullscreen msallowfullscreen width="668" height="376"></iframe>
-	      <script src='//fast.wistia.com/static/iframe-api-v1.js'></script>
-	    </center>
-    </div>
-  </div> <!-- /Video -->
-    
-  <div class="alert-header col-sm-10 col-sm-offset-1"><!-- Optin -->
-      <div class="row">
-        <h4 class="optin-h4">Subscribe below and I'll dare you to be great.</h4>
-      </div>
-	  <div class="row">
-	      <div class="top-optin col-sm-offset-4">
-	      
-	      <!-- Mailchimp -->
-			<form class="form-inline" id="before_header" action="//howdareyou.us8.list-manage.com/subscribe/post?u=4c05f20503bc3a9d84d1a63c9&amp;id=488afa7633" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-				<input type="email" value="" name="EMAIL" class="form-control email" id="mce-EMAIL" placeholder="Email address.." required>
-			<button id="header-optin-click" name="submit" onClick="ga('send', 'event', 'optin', 'subscribe', 'header optin');" class="submit btn btn-primary btn-lg" type="submit" value="Submit" tabindex="501">I Dare</button>
-			
-			</form> <!-- /Mailchimp -->
-	      </div>
-      </div>
-    </div>
-  </div>
-</div>
-<br />
-<hr>
+    <?php
+    do_action('get_header');
+    // Use Bootstrap's navbar if enabled in config.php
+    if (current_theme_supports('bootstrap-top-navbar') && is_category(podcast) ) {
+      get_template_part('templates/header-podcast');
+    } else if (current_theme_supports('bootstrap-top-navbar') && !is_page(podcast) ) {
+      get_template_part('templates/header-top-navbar');
+    } else {
+      get_template_part('templates/header');
+    }
+  ?>
   
 
   <section id="home" class="home jack-things">
@@ -136,8 +93,8 @@
       <div class="content row">
         <div class="col-sm-12">
 	          <h1>The new definition of Smart Home.</h1>
-          <h3>Pre-order your <span class="smart-home">SmartKit</span> on Indiegogo</h3>
-          <center><button type="button" class="btn btn-default security-btn">Back on Indiegogo</button></center>
+          <h3>Pre-order your <span class="smart-home">SmartKit</span> on Kickstarter</h3>
+          <center><button type="button" class="btn btn-default security-btn">Back on Kickstarter</button></center>
           <center><a href="" data-toggle="modal" data-target="#myModal">Virtual Walk-thru: How to pick the right equipment for your home</a></center>
           
         </div>
