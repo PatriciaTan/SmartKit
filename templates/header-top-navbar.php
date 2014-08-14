58ac49b61ea90c40d19ef82d696994ce2f1ca807
Mason L <mason.lawlor@me.com>
2014-08-14 00:38:43 -0700

    added new video and headlines

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -50,15 +50,14 @@
   <div class="col-sm-8 col-sm-offset-2"><!-- Video -->
 	  <div class="row"> 
 	    <center>
-	      <!--<iframe src="//fast.wistia.net/embed/iframe/ayvzkgovfs?videoFoam=true" allowtransparency="true" frameborder="0" scrolling="no" class="wistia_embed" name="wistia_embed" allowfullscreen mozallowfullscreen webkitallowfullscreen oallowfullscreen msallowfullscreen width="640" height="360"></iframe><script src="//fast.wistia.net/assets/external/iframe-api-v1.js"></script>-->
-	      <iframe src="//fast.wistia.net/embed/iframe/ayvzkgovfs?videoFoam=true" allowtransparency="true" frameborder="0" scrolling="no" class="wistia_embed" name="wistia_embed" allowfullscreen mozallowfullscreen webkitallowfullscreen oallowfullscreen msallowfullscreen width="640" height="360"></iframe><script src="//fast.wistia.net/assets/external/iframe-api-v1.js"></script>
+	      <iframe src="//fast.wistia.net/embed/iframe/6y2igsba97?videoFoam=true" allowtransparency="true" frameborder="0" scrolling="no" class="wistia_embed" name="wistia_embed" allowfullscreen mozallowfullscreen webkitallowfullscreen oallowfullscreen msallowfullscreen width="640" height="360"></iframe><script src="//fast.wistia.net/assets/external/iframe-api-v1.js"></script>
 	    </center>
     </div>
   </div> <!-- /Video -->
     
   <div class="alert-header col-sm-10 col-sm-offset-1"><!-- Optin -->
       <div class="row">
-        <h4 class="optin-h4">Subscribe below and I'll dare you to be great.</h4>
+        <h4 class="optin-h4">Be the first to know when we launch!</h4>
       </div>
 	  <div class="row">
 	      <div class="top-optin col-sm-offset-4">
@@ -66,7 +65,7 @@
 	      <!-- Mailchimp -->
 			<form class="form-inline" id="before_header" action="//howdareyou.us8.list-manage.com/subscribe/post?u=4c05f20503bc3a9d84d1a63c9&amp;id=488afa7633" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
 				<input type="email" value="" name="EMAIL" class="form-control email" id="mce-EMAIL" placeholder="Email address.." required>
-			<button id="header-optin-click" name="submit" onClick="ga('send', 'event', 'optin', 'subscribe', 'header optin');" class="submit btn btn-primary btn-lg" type="submit" value="Submit" tabindex="501">I Dare</button>
+			<button id="header-optin-click" name="submit" onClick="ga('send', 'event', 'optin', 'subscribe', 'header optin');" class="submit btn btn-primary btn-lg" type="submit" value="Submit" tabindex="501">Subscribe</button>
 			
 			</form> <!-- /Mailchimp -->
 	      </div>
