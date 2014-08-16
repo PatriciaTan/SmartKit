505e8b9019e3bd0ed47cfdc079733510860fd9a0
Mason L <mason.lawlor@me.com>
2014-08-16 00:07:50 -0700

    Revert d772cb8..c614513
    This rolls back to commit d772cb8b72a7c65600eaa036953556c709f4144e.

--- base-front-page.php
+++ base-front-page.php
@@ -7,66 +7,25 @@
     </div>
   <![endif]-->
 
-<header class="banner navbar navbar-default navbar-fixed-top" role="banner">
-  <div class="container">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="<?php echo home_url(); ?>/"><img src="http://masonlawlor.com//smartkit/assets/images/logo.png" alt="logo-nav" width="auto" height="48px" /></a>
-    </div>
-
-    <nav class="collapse navbar-collapse" role="navigation">
-    <button type="button" class="pull-right btn btn-default security-btn">Back on Indiegogo</button>
-    <ul class="nav navbar-nav navbar-right">
-      <li><a title="Lightning fast" href="#home" class="security-nav scroll">Home Security</a></li>
-      <li><a title="Get in touch!" href="#energy" class="scroll energy-nav">Smart Energy</a></li>
-      <li><a title="External page sample" href="#auto" class="scroll auto-nav">Home Automation</a></li>
-    </ul>
-    </nav>
-  </div>
-</header>
-
-
-  <section id="above-the-fold">
-  	<video id="videobcg" preload="auto" autoplay="true" loop="loop" muted="muted" volume="0">
-     		<source src="http://masonlawlor.com//smartkit/assets/video/timelapse.mp4" type="video/mp4">
-     		<source src="http://masonlawlor.com//smartkit/assets/video/timelapse.ogv" type="video/ogv">
-          Sorry, your browser does not support HTML5 video.
-	</video>
-    <div class="wrap container" role="document">
-      <div class="content row">
-        <div class="col-sm-12">
-          <h1>No Contracts.</h1>
-          <h3>How's that for a <span class="smart-home">Smart Home?</span></h3>
-          <div class="col-sm-4 jack-cols">
-              <a class="hero-sections" href="#"><img src="http://masonlawlor.com//smartkit/assets/images/security-hero.png" alt="the-athlete" />
-              <h4>Security</h4></a>
-          </div>
-          <div class="col-sm-4 jack-cols">
-              <a class="hero-sections" href="#"><img src="http://masonlawlor.com//smartkit/assets/images/energy-hero.png" alt="the-fit" />
-              <h4>Energy</h4></a>
-          </div>
-          <div class="col-sm-4 jack-cols">
-              <a class="hero-sections" href="#"><img src="http://masonlawlor.com//smartkit/assets/images/automation-hero.png" alt="the-restored" />
-              <h4>Automation</h4></a>
-          </div>
-          <center></center>
-          <a href="//fast.wistia.net/embed/iframe/1se5qohbsh?popover=true" class="wistia-popover[height=360,playerColor=7b796a,width=640]"><button type="button" class="btn btn-default hero-btn">Play Video</button></a>
-<script charset="ISO-8859-1" src="//fast.wistia.com/assets/external/popover-v1.js"></script>
-        </div>
-      </div><!-- /.content -->
-    </div><!-- /.wrap -->
-  </section>
-  
+    <?php
+    do_action('get_header');
+    // Use Bootstrap's navbar if enabled in config.php
+    if (current_theme_supports('bootstrap-top-navbar') && is_category(podcast) ) {
+      get_template_part('templates/header-podcast');
+    } else if (current_theme_supports('bootstrap-top-navbar') && !is_page(podcast) ) {
+      get_template_part('templates/header-top-navbar');
+    } else {
+      get_template_part('templates/header');
+    }
+  ?>
   
 
   <section id="home" class="home jack-things">
     <div class="wrap container" role="document">
       <div class="content row">
+      	<div class="col-sm-4 col-sm-offset-2">
+          	<img class="art img-responsive" src="http://masonlawlor.com/smartkit/assets/images/graphics/security.png" />
+        </div>
         <div class="col-sm-6"> 
           <h2><img class="security-icon" src="http://masonlawlor.com/smartkit/assets/images/security.png" /> Home Security</h2>
 		    <ul class="big-list">
@@ -74,13 +33,12 @@
 	          <li><img class="icons" src="http://masonlawlor.com/smartkit/assets/images/2-dev.png"/> Motion Detection</li>
 	          <li><img class="icons" src="http://masonlawlor.com/smartkit/assets/images/3-conversions.png"/> Smoke & CO Detection</li>
 	        </ul>
-          <div class="alert alert-t4tg">Starting at <span class="price-security">$34.99</span>/m</div>
-          <button class="btn btn-default btn-lg security-btn">Pre-order <i class="fa fa-chevron-right"></i></button>
+	        <div class="alert alert-t4tg">Starting at <span class="price-security">$34.99</span>/m</div>
         </div>
-        <div class="col-sm-6">
-          <center>
-          <img class="art img-responsive" src="http://masonlawlor.com/smartkit/assets/images/graphics/security.png" />
-          </center>
+      </div>
+      <div class="row">
+        <div class="col-sm-12">
+          <button class="btn btn-default btn-lg security-btn">Pre-order <i class="fa fa-chevron-right"></i></button>
         </div>
       </div><!-- /.content -->
     </div><!-- /.wrap -->
@@ -89,6 +47,9 @@
   <section id="energy" class="highlight home jack-things">
     <div class="wrap container" role="document">
       <div class="content row">
+      	<div class="col-sm-4 col-sm-offset-2">
+          <img class="art img-responsive" src="http://masonlawlor.com/smartkit/assets/images/graphics/energy.png" />
+        </div>
         <div class="col-sm-6">
           <h2><img class="energy-icon" src="http://masonlawlor.com/smartkit/assets/images/energy.png" /> Smart Energy</h2>
           <ul class="big-list">
@@ -97,12 +58,11 @@
 	          <li><img class="icons" src="http://masonlawlor.com/smartkit/assets/images/4-templates.png"/> Energy Saving Schedules</li>
 	        </ul>
           <div class="alert alert-t4tg">Starting at <span class="price-energy">$42.99</span>/m</div>
-          <button class="btn btn-default btn-lg energy-btn">Pre-order  <i class="fa fa-chevron-right"></i></button>
         </div>
-        <div class="col-sm-6">
-          <center>
-          <img class="art img-responsive" src="http://masonlawlor.com/smartkit/assets/images/graphics/energy.png" />
-          </center>
+      </div>
+      <div class="row">
+      	<div class="col-sm-12">
+          <button class="btn btn-default btn-lg energy-btn">Pre-order  <i class="fa fa-chevron-right"></i></button>
         </div>
       </div><!-- /.content -->
     </div><!-- /.wrap -->
@@ -111,6 +71,9 @@
   <section id="auto" class="home jack-things">
     <div class="wrap container" role="document">
       <div class="content row">
+      <div class="col-sm-4 col-sm-offset-2">
+          <img class="art img-responsive" src="http://masonlawlor.com/smartkit/assets/images/graphics/auto.png" />
+        </div>
         <div class="col-sm-6">
           <h2><img class="auto-icon" src="http://masonlawlor.com/smartkit/assets/images/automation.png" /> Home Automation</h2>
           <ul class="big-list">
@@ -119,28 +82,27 @@
 	          <li><img class="icons" src="http://masonlawlor.com/smartkit/assets/images/4-templates.png"/> Wifi Surveillance</li>
 	        </ul>
           <div class="alert alert-t4tg">Starting at <span class="price-auto">$56.99</span>/m</div>
-          <button class="btn btn-default btn-lg auto-btn">Pre-order  <i class="fa fa-chevron-right"></i></button>
         </div>
-        <div class="col-sm-6">
-          <center>
-          <img class="art img-responsive" src="http://masonlawlor.com/smartkit/assets/images/graphics/auto.png" />
-          </center>
+      </div>
+      <div class="row">
+      	<div class="col-sm-12">
+          <button class="btn btn-default btn-lg auto-btn">Pre-order  <i class="fa fa-chevron-right"></i></button>
         </div>
       </div><!-- /.content -->
     </div><!-- /.wrap -->
   </section>
   
+<hr>
+  
   <section id="bottom-cta" class="home">
     <div class="wrap container" role="document">
       <div class="content row">
         <div class="col-sm-12">
-	          <h1>The new definition of Smart Home.</h1>
-          <h3>Pre-order your <span class="smart-home">SmartKit</span> on Indiegogo</h3>
-          <center><button type="button" class="btn btn-default security-btn">Back on Indiegogo</button></center>
+	          <h1>Rationality meets smart home.</h1>
+          <h3>Pre-order your <span class="smart-home">SmartKit</span> on Kickstarter</h3>
+          <center><button type="button" class="btn btn-default cta-btn blog-cta">Back on Kickstarter</button></center>
           <center><a href="" data-toggle="modal" data-target="#myModal">Virtual Walk-thru: How to pick the right equipment for your home</a></center>
-          
         </div>
-        <i class="fa fa-check"></i>
       </div><!-- /.content -->
     </div><!-- /.wrap -->
   </section>
@@ -148,27 +110,82 @@
 
   <?php get_template_part('templates/footer'); ?>
   
-  <!-- pop-up modal -->
+<!-- pop-up modal -->
+<script language="JavaScript">
+  <!--
+    function _submit_form(frm){
+      var email = frm.email.value;
+      var valid_email = /^([a-zA-Z0-9_.-])+@(([a-zA-Z0-9-])+.)+([a-zA-Z0-9]{2,4})+$/;
+      
+      if( valid_email.test( email ) ){
+        document.getElementById('before').style.display = 'none';
+        document.getElementById('after').style.display = 'block';
+        return true;
+      } else {
+        alert("Invalid email address");
+      }
+      return false;
+    }
+//-->
+</script>
 
-    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+<script type="text/javascript"
+        src="//cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.3.1/jquery.cookie.min.js"></script>
+  <script type="text/javascript">
+    function showPopup() {
+      $('#myModal').modal('show');
+    }
+
+    function timeOut() {
+      if ($.cookie('foreverjobless') !== '1') {
+        setTimeout(function() {
+          showPopup();
+          jQuery.cookie('foreverjobless', '1', { expires: 7 });
+        }, 240000);
+      }
+    }
+
+    $(document).ready(function () {
+      timeOut();
+      stickyElement();
+    });
+
+</script>
+
+   <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
       <div class="modal-dialog">
         <div class="modal-content">
-          <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-            <center><h3 class="modal-title" id="myModalLabel"><span class="smart-home">VIDEO TRAINING LIBRARY</span></h3></center>
-          </div>
+          <!--<button type="button" class="close" style="margin-right: 10px;margin-top: 5px;" data-dismiss="modal" aria-hidden="true">&times;</button>-->
           <div class="modal-body">
-            <center><h3>Equip yourself like a pro</h3><br>
-            <p>Over the next 5 days you'll receive 5 videos, giving you the expertise to simplify your life and protect the things you love.</p></center>
-            <br />
-            <center><form class="form-inline" role="form">
-                <input class="required" style="font-style:italic;" placeholder=" your email ..."></input>
-                <button type="submit" class="submit btn" value="download">Send it my way</button>
-            </form></center><br>
+    			
+    			<div class="col-sm-10 col-sm-offset-1 modal-video"><!-- Video -->
+				 	<div class="row"> 
+					 	<center>
+					 		<iframe src="//fast.wistia.net/embed/iframe/6y2igsba97?videoFoam=true" allowtransparency="true" frameborder="0" scrolling="no" class="wistia_embed" name="wistia_embed" allowfullscreen mozallowfullscreen webkitallowfullscreen oallowfullscreen msallowfullscreen width="640" height="360"></iframe><script src="//fast.wistia.net/assets/external/iframe-api-v1.js"></script>
+					 		<script src='//fast.wistia.com/static/iframe-api-v1.js'></script>
+					 	</center>
+					</div>
+				</div> <!-- /Video -->
+    		</div>
+    	<br>	
+        <h4 class="optin-h4">stupid bug, buggin me </h4><!-- optin -->
+        <h4 class="">Be the first to know when we launch!!</h4><!-- optin -->
+	  <div class="row">
+	      <div class="top-optin col-sm-offset-3">
+	      
+	      <!-- Mailchimp -->
+			<form class="form-inline" id="before_header" action="//smartk.us8.list-manage.com/subscribe/post?u=99a776394eb3b3549e7c5e53c&amp;id=628848161c" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+				<input type="email" value="" name="EMAIL" class="form-control email" id="mce-EMAIL" placeholder="Email address.." required>
+			<button id="header-optin-click" name="submit" onClick="ga('send', 'event', 'optin', 'subscribe', 'header optin');" class="submit btn btn-primary btn-lg" type="submit" value="Submit" tabindex="501">Subscribe</button>
+			
+			</form> <!-- /Mailchimp -->
+	      </div><!-- /optin -->
+		  <br><br>
           </div>
         </div>
       </div>
-    </div><!-- /pop-up modal -->
+    </div>  
+<!-- /pop-up modal -->
     
 	
 	</body>
--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -1,4 +1,4 @@
-<header class="banner navbar navbar-default navbar-fixed-top" role="banner">
+<header class="banner navbar navbar-default navbar-static-top" role="banner">
   <div class="container">
     <div class="navbar-header">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
@@ -7,17 +7,32 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="<?php echo home_url(); ?>/"><img src="http://www.jackalopemedia.com/t4tg/logo-nav.png" alt="logo-nav" width="58" height="auto" /></a>
+      <a class="navbar-brand" href="<?php echo home_url(); ?>/"><img src="http://masonlawlor.com//smartkit/assets/images/logo.png" alt="logo-nav" width="auto" height="48px" /></a>
     </div>
 
     <nav class="collapse navbar-collapse" role="navigation">
-    <button type="button" class="pull-right btn btn-default navbar-btn navbar-btn">30 Day Free Trial</button>
+    <button type="button" class="pull-right btn btn-default nav-btn">Back on Kickstarter</button>
       <?php
         if (has_nav_menu('primary_navigation')) :
           wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav'));
         endif;
       ?>
-      
     </nav>
   </div>
 </header>
+
+  <section id="bottom-cta" class="home">
+    <div class="wrap container" role="document">
+      <div class="content row">
+        <div class="col-sm-12">
+	          <h1>Rationality meets smart home.</h1>
+          <h3>No contracts.</h3>
+          <center><button type="button" class="btn btn-default cta-btn blog-cta">Play video</button></center>
+          <center><a href="" data-toggle="modal" data-target="#myModal">Virtual Walk-thru: How to pick the right equipment for your home</a></center>
+        </div>
+      </div><!-- /.content -->
+    </div><!-- /.wrap -->
+  </section>
+<br />
+<hr>
+
